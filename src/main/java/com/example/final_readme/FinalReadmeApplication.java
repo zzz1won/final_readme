c53650e9429c941313aac4850df9ea3c610d3962
zzz1won <92144729+zzz1won@users.noreply.github.com>
2022-01-27 10:56:17 +0900

    read Me 업로드용

---
+++ src/main/java/com/example/final_readme/FinalReadmeApplication.java
@@ -0,0 +1,13 @@
+package com.example.final_readme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalReadmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalReadmeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/final_readme/FinalReadmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.final_readme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalReadmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
